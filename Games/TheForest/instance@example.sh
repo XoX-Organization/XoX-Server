4210daed5b521bc155bad166eb3ee4ed9e63491d
Pom Pom <2003victoryy@gmail.com>
2023-11-28 13:41:02 +0800

    Add The Forest game support and instance script

---
+++ Games/TheForest/instance@example.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Your instance name
+# MUST be UNIQUE for each instance
+INSTANCE_NAME=""
+
+# Beta Branch
+APP_BETA_BRANCH="none"
+
+# Steam user for updates and workshop updates
+# Either "anonymous" or your Steam username
+STEAM_USERNAME="victorwong03"
---
+++ Games/start@TheForest.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+source "./start@~.sh"
+echo -e "TheForest Server"
+
+APP_ID=892970
+
+if ! setWorkingDirectory "./TheForest";
+then
+    exit 1
+fi
+
+findInstances
+
+if ! printInstances;
+then
+    exit 1
+fi
+
+if ! chooseInstance;
+then
+    exit 1
+fi
+
+if ! loadInstance;
+then
+    exit 1
+fi
+
+NAME="TheForest@$INSTANCE_NAME"
+
+if checkScreenInstanceExists "$NAME";
+then
+    exit 1
+fi
+
+if [[ "$*" == *-noupdate* ]];
+then
+    echo -e "\nSkipping update."
+else
+    updateApp
+fi
+
+if checkScreenInstanceExists "$NAME";
+then
+    exit 1
+fi
+
+cd "$SOURCE_PATH"
+
+startScreenInstance "$NAME" ""
+
+read -p $'\n\nPress Enter to attach to the instance, or Ctrl+C to skip.'
+screen -r "$NAME"
+exit 0
