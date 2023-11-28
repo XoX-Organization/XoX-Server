31885f837ebc3766984b5fc11d096f3be1983e7e
Pom Pom <2003victoryy@gmail.com>
2023-11-28 13:09:29 +0800

    Add Valheim server configuration files

---
+++ Games/Valheim/instance@example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Your instance name
+# MUST be UNIQUE for each instance
+INSTANCE_NAME=""
+
+# Server Variables
+SERVER_NAME="XoX eSports Official Server"
+SERVER_PORT="2456"
+SERVER_WORLD=""
+SERVER_PASSWORD="xoxsince2020"
+
+# Beta Branch
+APP_BETA_BRANCH="none"
+
+# Steam user for updates and workshop updates
+# Either "anonymous" or your Steam username
+STEAM_USERNAME="victorwong03"
---
+++ Games/start@Valheim.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+source "./start@~.sh"
+
+cd "./Valheim"
+echo -e "Valheim Server"
+
+APP_ID=892970
+SOURCE_PATH="$HOME/.steam/steam/steamapps/common/Valheim dedicated server"
+
+SERVER_NAME=
+SERVER_PORT=
+SERVER_WORLD=
+SERVER_PASSWORD=
+
+findInstances
+
+if ! printInstances;
+then
+    exit 1
+fi
+
+if ! chooseInstance;
+then
+    exit 1
+fi
+
+if ! loadInstance;
+then
+    exit 1
+fi
+
+NAME="Valheim@$INSTANCE_NAME"
+
+if checkScreenInstanceExists "$NAME";
+then
+    exit 1
+fi
+
+if [[ "$*" == *-noupdate* ]];
+then
+    echo -e "\nSkipping update."
+else
+    updateApp
+fi
+
+if checkScreenInstanceExists "$NAME";
+then
+    exit 1
+fi
+
+cd "$SOURCE_PATH"
+
+startScreenInstance "$NAME" "${SOURCE_PATH}/valheim_server.x86_64" \
+    -name "$SERVER_NAME" \
+    -port "$SERVER_PORT" \
+    -world "$SERVER_WORLD" \
+    -password "$SERVER_PASSWORD" \
+    -public 0 \
+    -nographics \
+    -batchmode \
+    -crossplay
+
+read -p $'\n\nPress Enter to attach to the instance, or Ctrl+C to skip.'
+screen -r "$NAME"
+exit 0
