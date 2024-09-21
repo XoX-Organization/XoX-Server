ad9bd0cd6e071f5f5d7d7ab1729e241c20711dde
Pom Pom <2003victoryy@gmail.com>
2024-09-21 06:14:41 +0000

    Add time elapsed on stream operations

--- src/utilities/pipe.ts
+++ src/utilities/pipe.ts
@@ -1,8 +1,24 @@
 import { Writable } from "stream"
 
 export class CarriageReturnWritableStream extends Writable {
-    _write(chunk: any, encoding: string, callback: Function) {
-        chunk
+    private startTime: number
+    private timeoutId?: NodeJS.Timeout
+
+    constructor() {
+        super()
+        this.startTime = Date.now()
+    }
+
+    _write = (chunk?: any, encoding?: string, callback?: Function, ended?: boolean) => {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId)
+            this.timeoutId = undefined
+        }
+        if (!this.timeoutId && !this.writableFinished && !ended) {
+            this.timeoutId = setTimeout(() => this._write(chunk), 100)
+        }
+        const elapsed = ((Date.now() - this.startTime) / 1000).toFixed(1)
+        ;(chunk ?? "")
             .toString()
             .split("\n")
             .forEach((line: string) => {
@@ -10,12 +26,19 @@ export class CarriageReturnWritableStream extends Writable {
                 if (message.length <= 0) {
                     return
                 }
+                process.stdout.write(`\x1b[90m`)
                 process.stdout.write(
-                    `\r> ${message.slice(0, process.stdout.columns - 10)}`.padEnd(
+                    `\r> [${elapsed}s] ${message.slice(0, process.stdout.columns - 20)}`.padEnd(
                         process.stdout.columns + 1,
                     ),
                 )
+                process.stdout.write(`\x1b[0m`)
             })
-        callback()
+        callback?.()
+    }
+
+    end = (chunk?: any, encoding?: any, cb?: any) => {
+        this._write(chunk, encoding, cb, true)
+        return this
     }
 }
